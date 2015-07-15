fe0992e1800cedc46c8c9f36c387a010eb6c0787
Marlon <Marlon@Marlon-PC>
2015-07-14 19:03:27 -0600

    Segunda Modificacion

--- HojaTrabajo1/src/hojatrabajo1/Driver.java
+++ HojaTrabajo1/src/hojatrabajo1/Driver.java
@@ -8,16 +8,19 @@ public class Driver {
 	Scanner leer = new Scanner(System.in);
 	Radio radio1= new RadioCarro();
 	radio1.setEstado(false);
+        radio1.setAMFM(1);
 
 	boolean nEstado;
-	String indicador;
 	int banda;
 	boolean ud;
 	int pos;
+        int b=0;
 
 	while (a == 0){
             nEstado = radio1.getEstado();
-            if (nEstado==true){System.out.println("\nRADIO ENCENDIDO\n");}
+            banda = radio1.getAMFM();
+            if (nEstado==true && banda==1 && b==1){System.out.println("\nRADIO ENCENDIDO\nFrecuencia: FM");}
+            else if (nEstado==true && banda==0 && b==1){System.out.println("\nRADIO ENCENDIDO\nFrecuencia: AM");}
             else if (nEstado==false){System.out.println("\nRADIO APAGADO\n");}
 
             System.out.println("1. On\n2. Cambiar AM o FM \n3. Cambiar emisora con dial\n4. Guardar emisora\n5. Cambiar emisora con boton\n6. Off\n7. Salir del programa\n\nNumero: ");
@@ -26,17 +29,19 @@ public class Driver {
             if (opcion.equals("1")){
 		nEstado = true;
 		radio1.setEstado(nEstado);
+                b=1;
             }
-            else if (opcion.equals("2")){
+            else if (opcion.equals("2") && b==1){
+		if (banda==1){banda=0; radio1.setAMFM(banda);}
+                else if (banda==0){banda=1; radio1.setAMFM(banda);}
+            }
+            else if (opcion.equals("3") && b==1){
 		System.out.println("e");
             }
-            else if (opcion.equals("3")){
+            else if (opcion.equals("4") && b==1){
 		System.out.println("e");
             }
-            else if (opcion.equals("4")){
-		System.out.println("e");
-            }
-            else if (opcion.equals("5")){
+            else if (opcion.equals("5") && b==1){
 		System.out.println("e");
             }
             else if (opcion.equals("6")){
@@ -48,7 +53,7 @@ public class Driver {
 		a = 1;
             }
             else {
-		System.out.println("\nERROR. La opcion ingresada no es valida, recuerde que solamente puede ingresar numeros de 1 a 7. Intente otra vez.\n");
+		System.out.println("\nERROR. La opcion ingresada no es valida, recuerde que solamente puede ingresar numeros de 1 a 7 \ny si el radio esta apagado no puede utilizar las opciones 2 a 5. Intente otra vez.\n");
             }
 	}
 		
